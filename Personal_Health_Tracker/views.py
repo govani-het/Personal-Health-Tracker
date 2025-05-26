73f8e2466517c50959d312c148f240c6392d75fd
Het <govanihet09@gmail.com>
2025-05-26 13:55:45 +0530

    first commit

---
+++ Personal_Health_Tracker/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render, redirect
+
+
+def index(request):
+    return render(request, 'login.html')
+
+def register(request):
+    return render(request, 'register.html')
